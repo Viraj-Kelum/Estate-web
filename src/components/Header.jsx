beff274468a59c2c99d1dde80235b702c6c6ac82
VirajKelum <workmail12311@gmail.com>
2025-01-08 17:12:07 +0530

    Update project with recent changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div
       className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
-      style={{ backgroundImage: "url(src/assets/header_img.png)" }}
+      style={{ backgroundImage: "url(/header_img.png)" }}
       id="Header"
     >
       <Navbar />
